f163f54951d45205ee66da8f7cae45180388e54a
Gerson Azabache <gerson_dunk@hotmail.com>
2020-11-22 23:22:25 -0500

    Métodos Get, post, put y delete completados para el controlador Autor

--- APIBiblioteca/Controllers/AutorController.cs
+++ APIBiblioteca/Controllers/AutorController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using APIBiblioteca.Context;
+using APIBiblioteca.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +13,65 @@ namespace APIBiblioteca.Controllers
     [ApiController]
     public class AutorController:ControllerBase
     {
+        private readonly ApplicationDBContext context;
 
+        public AutorController(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Autor>> Get()
+        {
+            return context.Autor.ToList();
+        }
+
+        [HttpGet("{id}",Name ="ObtenerAutor")]
+        public ActionResult<Autor>Get(int id)
+        {
+            var autor = context.Autor.FirstOrDefault(x => x.Id == id);
+            if(autor == null)
+            {
+                return NotFound();
+            }
+            return autor;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Autor autor)
+        {
+            context.Autor.Add(autor);
+            context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObtenerAutor", new { id = autor.Id }, autor);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put([FromBody] Autor autor, int id)
+        {
+            if(id != autor.Id)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(autor).State = EntityState.Modified;
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Autor> Delete(int id)
+        {
+            var autor = context.Autor.FirstOrDefault(x => x.Id == id);
+
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            context.Entry(autor).State = EntityState.Deleted;
+            context.SaveChanges();
+            return autor;
+        }
     }
 }
--- APIBiblioteca/Entities/Autor.cs
+++ APIBiblioteca/Entities/Autor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace APIBiblioteca.Entities
     public class Autor
     {
         public int Id { get; set; }
+        [Required]
         public string Nombre { get; set; }
     }
 }
