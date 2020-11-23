95ac9c40e16397fe5ba0099faec228f294654ace
Gerson Azabache <gerson_dunk@hotmail.com>
2020-11-22 22:34:17 -0500

    EF configurado y primera migracion creada

--- APIBiblioteca/Context/ApplicationDBContext.cs
+++ APIBiblioteca/Context/ApplicationDBContext.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using APIBiblioteca.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace APIBiblioteca.Context
 {
-    public class AplicationDBContext
+    public class ApplicationDBContext : DbContext
     {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Autor> Autor { get; set; }
+
     }
 }
